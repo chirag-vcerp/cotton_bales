2c433cc39a623d3b5dcc85fdc2584c3fee8322f6
Chirag Modi <chirag.modi@vc-erp.com>
2025-05-06 19:08:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ from openai import OpenAI
 from dotenv import load_dotenv
 
 load_dotenv()
-OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
